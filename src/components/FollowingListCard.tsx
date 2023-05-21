4646503156c8a47740150e12453e31157ea1a8ae
surya vivek <suryavivek33377@gmail.com>
2023-05-21 23:40:47 +0530

    Renders profile page follow button the right way.

--- src/components/FollowingListCard.tsx
+++ src/components/FollowingListCard.tsx
@@ -1,6 +1,6 @@
 const FollowingListCard = () => {
   return (
-    <div className="mr-8 mt-8 flex flex-col items-center space-y-4 rounded-md border border-accent-6 bg-black p-6">
+    <div className="mr-8 flex flex-col items-center space-y-4 rounded-md border border-accent-6 bg-black p-6">
       People you follow
     </div>
   );
--- src/pages/[username]/index.tsx
+++ src/pages/[username]/index.tsx
@@ -11,10 +11,13 @@ import FollowingListCard from "~/components/FollowingListCard";
 import Image from "next/image";
 import { api } from "~/utils/api";
 import PostComponent from "~/components/Post";
+import { getSession } from "next-auth/react";
+import { type FC } from "react";
 
-export const getServerSideProps: GetServerSideProps<{ user: User }> = async (
-  ctx: GetServerSidePropsContext
-) => {
+export const getServerSideProps: GetServerSideProps<{
+  user: User;
+  isFollowed: boolean | "own";
+}> = async (ctx: GetServerSidePropsContext) => {
   const username = ctx.params?.username as string;
 
   const user = await prisma.user.findUnique({
@@ -29,15 +32,59 @@ export const getServerSideProps: GetServerSideProps<{ user: User }> = async (
     };
   }
 
+  const session = await getSession(ctx);
+
+  if (!session) {
+    return {
+      props: {
+        user,
+        isFollowed: false,
+      },
+    };
+  }
+
+  if (session.user.username === user.username) {
+    return {
+      props: {
+        user,
+        isFollowed: "own",
+      },
+    };
+  }
+
+  const follow = await prisma.follows.findUnique({
+    where: {
+      followerUsername_followingUsername: {
+        followerUsername: session.user.username,
+        followingUsername: user.username,
+      },
+    },
+  });
+
+  console.log({ follow });
+
+  if (!follow) {
+    console.log("returning false due to null");
+    return {
+      props: {
+        user,
+        isFollowed: false,
+      },
+    };
+  }
+  console.log("returning true due to follow");
+
   return {
     props: {
       user,
+      isFollowed: true,
     },
   };
 };
 
 const ProfilePage = ({
   user,
+  isFollowed,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <Layout>
@@ -58,9 +105,7 @@ const ProfilePage = ({
                 <h2 className="text-2xl font-bold capitalize">{user.name}</h2>
                 <p className="text-sm">@{user.username}</p>
               </div>
-              <button className="rounded-md bg-accent-8 px-4 py-2 font-bold text-accent-2 hover:brightness-75">
-                Follow
-              </button>
+              <FollowBtn isFollowed={isFollowed} />
             </div>
             <p className="py-4">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -75,12 +120,32 @@ const ProfilePage = ({
           <UserPosts user={user} />
         </div>
 
-        <div className="w-1/3">
+        <div className="w-1/3 space-y-8">
           <ProfileCard />
           <FollowingListCard />
         </div>
       </>
     </Layout>
+  );
+};
+
+type FollowBtnProps = {
+  isFollowed: boolean | "own";
+};
+
+const FollowBtn: FC<FollowBtnProps> = ({ isFollowed }) => {
+  if (isFollowed === "own") {
+    return (
+      <button className="rounded-md bg-accent-8 px-4 py-2 font-bold text-accent-2 hover:brightness-75">
+        Edit profile
+      </button>
+    );
+  }
+
+  return (
+    <button className="rounded-md bg-accent-8 px-4 py-2 font-bold text-accent-2 hover:brightness-75">
+      {isFollowed ? "unfollow" : "follow"}
+    </button>
   );
 };
 
