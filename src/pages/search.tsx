15eb4950160c808267e2ea834035edeb38d10efc
suryavivek <suryavivek33377@gmail.com>
2023-07-01 20:03:43 +0530

    fix: search page height

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -5,7 +5,7 @@ import Search from "~/components/Search";
 const SearchPage: NextPage = () => {
   return (
     <Layout>
-      <div className="mr-8 h-[calc(100vh-10rem)] w-full overflow-hidden rounded-md border-blue-2 bg-white p-4 dark:border-accent-6 dark:bg-black md:border md:p-8">
+      <div className="mr-8 h-[calc(100vh-10rem)] w-full rounded-md border-blue-2 bg-white p-4 dark:border-accent-6 dark:bg-black md:h-[calc(100vh-9rem)] md:border md:p-8">
         <Search />
       </div>
     </Layout>
